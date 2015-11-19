5db243f22963311771ed8d1aa4acc91f24b8356a
Eleonore9 <eleonore.mayola@ymail.com>
2015-11-19 15:11:37 +0000

    Adding logging + proper aws cred

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
+(def slf4j-version "1.7.12")
 (defproject kixi.hecuba.minorca "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
@@ -9,7 +10,14 @@
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.csv  "0.1.3"]
                  [amazonica             "0.3.39"]
-                 [org.clojure/tools.logging "0.3.1"]]
+                 ;; Logging
+                 [org.clojure/tools.logging "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.2"]
+                 [org.slf4j/jul-to-slf4j         ~slf4j-version]
+                 [org.slf4j/jcl-over-slf4j       ~slf4j-version]
+                 [org.slf4j/log4j-over-slf4j     ~slf4j-version]
+
+                 ]
   :plugins [[lein-cljfmt     "0.1.11"]
             [jonase/eastwood "0.2.1"]
             [lein-kibit      "0.1.2"]])
